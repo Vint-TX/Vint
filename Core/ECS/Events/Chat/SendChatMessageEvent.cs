6da24447de0475028dfb255104f2e346f5d1d9ec
ItalianStyle <lorenzo.petraroli@hotmail.it>
2024-04-23 20:03:42 +0200

    Implemented chat censorship feature

--- Core/ECS/Events/Chat/SendChatMessageEvent.cs
+++ Core/ECS/Events/Chat/SendChatMessageEvent.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using Vint.Core.ChatCommands;
 using Vint.Core.Config;
 using Vint.Core.Database.Models;
@@ -7,12 +8,14 @@ using Vint.Core.ECS.Entities;
 using Vint.Core.Protocol.Attributes;
 using Vint.Core.Server;
 using Vint.Core.Utils;
+using YamlDotNet.Core;
 
 namespace Vint.Core.ECS.Events.Chat;
 
 [ProtocolId(1446035600297)]
 public class SendChatMessageEvent : IServerEvent {
     static Dictionary<string, ChatConfigComponent> ConfigPathToConfig { get; } = new();
+
     public string Message { get; private set; } = null!;
 
     public void Execute(IPlayerConnection sender, IEnumerable<IEntity> entities) {
@@ -35,21 +38,35 @@ public class SendChatMessageEvent : IServerEvent {
         }
 
         Message = Message.Trim();
-
-        if (!Validate(chat.TemplateAccessor!.ConfigPath!, Message)) {
-            sender.Logger.ForType(GetType()).Warning("Message failed validation: '{Message}'", Message);
-            return;
+        string checkingMessage = Message;
+        if (!Validate(chat.TemplateAccessor!.ConfigPath!, ref checkingMessage)) {
+            sender.Logger.ForType(GetType()).Warning($"Message failed validation: '{Message}', using {checkingMessage}");
         }
-
+        Message = checkingMessage;
         ChatUtils.SendMessage(Message, chat, ChatUtils.GetReceivers(sender, chat), sender);
     }
 
     // todo ban-words (C6OI: I dont want to implement it, but I dont mind if someone sends a PR with it)
-    static bool Validate(string chatConfigPath, string message) {
+    static bool Validate(string chatConfigPath, ref string message) {
         ChatConfigComponent chatConfig = GetChatConfig(chatConfigPath);
+        bool isCleanMessage = true;
 
-        return message.Length > 0 && message.Length <= chatConfig.MaxMessageLength;
+        if (ConfigManager.BadWordsPatterns is not null) {
+            foreach (var entry in ConfigManager.BadWordsPatterns) {
+                string forbiddenWord = entry.Key;
+                string forbiddenWordPattern = entry.Value;
+
+                // Use Regex to detect the word with mixed scripts
+                if (Regex.IsMatch(message, forbiddenWordPattern, RegexOptions.IgnoreCase)) {
+                    isCleanMessage = false;
+                    string maskedWord = new string('*', forbiddenWord.Length);
+                    message = Regex.Replace(message, forbiddenWordPattern, maskedWord, RegexOptions.IgnoreCase);
+                }
+            }
+        }
+        return message.Length > 0 && message.Length <= chatConfig.MaxMessageLength && isCleanMessage;
     }
+
 
     [SuppressMessage("ReSharper", "InvertIf")]
     static ChatConfigComponent GetChatConfig(string path) {
