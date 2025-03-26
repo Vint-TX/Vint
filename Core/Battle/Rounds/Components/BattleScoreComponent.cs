fa9fc7b668068c89c15527c81933ab377ef7e260
C6OI <C6OI@yandex.ru>
2025-03-26 18:23:29 +0500

    Vint-28

--- Core/Battle/Rounds/Components/BattleScoreComponent.cs
+++ Core/Battle/Rounds/Components/BattleScoreComponent.cs
@@ -4,7 +4,4 @@ using Vint.Core.Server.Game.Protocol.Attributes;
 namespace Vint.Core.Battle.Rounds.Components;
 
 [ProtocolId(1436532217083)]
-public class BattleScoreComponent : IComponent {
-    public int ScoreRed { get; private set; }
-    public int ScoreBlue { get; private set; }
-}
+public class BattleScoreComponent : IComponent;
