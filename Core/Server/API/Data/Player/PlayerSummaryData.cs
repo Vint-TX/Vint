a999d4defcb4b7250ce0fd6c38c515ade8b99e97
C6OI <C6OI@yandex.ru>
2025-05-23 20:35:56 +0500

    add country code to PlayerSummaryData

--- Core/Server/API/Data/Player/PlayerSummaryData.cs
+++ Core/Server/API/Data/Player/PlayerSummaryData.cs
@@ -3,10 +3,12 @@ namespace Vint.Core.Server.API.Data.Player;
 public record PlayerSummaryData(
     long Id,
     string Username,
-    string Email
+    string Email,
+    string CountryCode
 ) {
     public static PlayerSummaryData FromPlayer(Database.Models.Player player) =>
         new(player.Id,
             player.Username,
-            player.Email);
+            player.Email,
+            player.CountryCode);
 }
