dfd9a65a6fc618c9716776f24f274ad9a427755b
C6OI <C6OI@yandex.ru>
2025-05-27 18:02:30 +0500

    bugfixes

--- Core/Server/API/Controllers/NewsController.cs
+++ Core/Server/API/Controllers/NewsController.cs
@@ -1,20 +1,47 @@
 using LinqToDB;
 using Microsoft.Extensions.DependencyInjection;
 using Vint.Core.Database;
+using Vint.Core.Email.Components;
 using Vint.Core.Server.API.Attributes;
 using Vint.Core.Server.API.Data;
 using Vint.Core.Server.API.Data.Player;
 using Vint.Core.Server.API.Data.Status;
 using Vint.Core.Server.API.Utils;
+using Vint.Core.Server.Game;
 
 namespace Vint.Core.Server.API.Controllers;
 
 public class NewsController(
+    GameServer server,
     IServiceScopeFactory serviceScopeFactory
 ) : IApiController {
+    [MessageId(47)]
+    public async Task<IClientDTO> UnsubscribeFromNewsletter(long playerId, string token) {
+        await using (DbConnection db = new()) {
+            bool success = await db.Players
+                .Where(player => player.Id == playerId && player.NewsletterUnsubscribeToken == token)
+                .Set(player => player.NewsletterSubscribed, false)
+                .Set(player => player.NewsletterUnsubscribeToken, (string?)null)
+                .UpdateAsync() > 0;
+
+            if (!success)
+                return ErrorDTO.NotFound("Invalid token or player id");
+        }
+
+        IPlayerConnection? connection = server.FindConnection(playerId);
+
+        if (connection != null) {
+            connection.Player.NewsletterSubscribed = false;
+            connection.Player.NewsletterUnsubscribeToken = null;
+            await connection.UserContainer.Entity.ChangeComponent<UserSubscribeComponent>(component => component.Subscribed = false);
+        }
+
+        return SuccessDTO.NoContent();
+    }
+
     [MessageId(48)]
     public async Task<IClientDTO> GetSubscribers(string countryCode, int from, int count = 500) {
-        from = Math.Max(0, from - 1);
+        from = Math.Max(0, from);
 
         await using DbConnection db = new();
         List<PlayerSummaryData> subscribers = await db.Players
